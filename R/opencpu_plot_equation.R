5562665a2d12903f3faf67e13964150bc62055d7
swt02026 <swt02026@yahoo.com.tw>
2015-06-16 08:37:40 +0800

    initial
    first commit

---
+++ R/opencpu_plot_equation.R
@@ -0,0 +1,33 @@
+library(ggplot2)
+
+draw<-function(input,min_input,max_input,step_input){
+  
+  min = eval(
+    expr = parse(
+      text=min_input
+    )
+  )
+  max = eval(
+    expr = parse(
+      text=max_input
+    )
+  )
+  step = eval(
+    expr = parse(
+      text=step_input
+    )
+  )
+  
+  x = seq(min,max)
+  
+  y =  eval(
+    expr = parse(
+      text=input
+    )
+  )
+  
+  df = data.frame(x = as.vector(x),y = as.vector(y))
+  
+  p<-ggplot(data=df,aes(x=x,y=y)) + geom_line(colour="green",size = 1)+ geom_point()
+  p
+}
